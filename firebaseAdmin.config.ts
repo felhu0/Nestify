51f5beb024c46a8673881a2aa666cd80b22f0742
Felicia Hu <feliciahu1992_@hotmail.com>
2024-11-12 22:04:52 +0100

    Refactor Firebase admin configuration to use service account from environment variable

--- firebaseAdmin.config.ts
+++ firebaseAdmin.config.ts
@@ -1,12 +1,29 @@
 
-import * as admin from "firebase-admin";
-import serviceAccount from "./serviceAccountKey.json";
+// import * as admin from "firebase-admin";
+// import serviceAccount from "./serviceAccountKey.json";
 
-const serviceAccountConfig: admin.ServiceAccount = serviceAccount as admin.ServiceAccount;
+// const serviceAccountConfig: admin.ServiceAccount = serviceAccount as admin.ServiceAccount;
+
+// if (!admin.apps.length) {
+//   admin.initializeApp({
+//     credential: admin.credential.cert(serviceAccountConfig), 
+//     projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+//   });
+// }
+
+// const db = admin.firestore();
+// const auth = admin.auth();
+
+// export { db, auth };
+
+import * as admin from "firebase-admin";
+
+// Hämta servicekontokonfigurationen från en miljövariabel
+const serviceAccountConfig = JSON.parse(process.env.SERVICE_ACCOUNT_KEY || "{}");
 
 if (!admin.apps.length) {
   admin.initializeApp({
-    credential: admin.credential.cert(serviceAccountConfig), 
+    credential: admin.credential.cert(serviceAccountConfig),
     projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
   });
 }
@@ -15,3 +32,4 @@ const db = admin.firestore();
 const auth = admin.auth();
 
 export { db, auth };
+
